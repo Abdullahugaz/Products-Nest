33fb7d6790bfbe9cd605f05d046382106cf32c1d
Abdullahugaz <abdullahugaz@gmail.com>
2025-08-26 10:03:13 +0300

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 7000); 
+  await app.listen(process.env.PORT ?? 7000);
 }
-bootstrap();
+bootstrap().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
